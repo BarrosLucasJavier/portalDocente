705050d8800ffa0e82e33e5a3c80736396d7e2ff
Barros Lucas <lucasbcco@gmail.com>
2025-06-29 19:22:56 -0300

    footer finalizado, se eliminaron pestañas

--- js/panelCarga.js
+++ js/panelCarga.js
@@ -4,27 +4,7 @@ import { cursosLetra, cursosNumero } from "../assets/data/cursos.js";
 import { completo } from "./validaciones.js";
 import { limpiar } from "./funciones.js";
 
-/* Pestañas */
-const cargaCurso = document.getElementById('cargaCurso');
-const cargaEscuelas = document.getElementById('cargaEscuelas');
-const cargaAlumno = document.getElementById('cargaAlumno');
 
-cargaEscuelas.addEventListener('click',()=>{
-    cargaEscuelas.classList.add('posicionAdelante');
-    cargaCurso.classList.remove('posicionAdelante');
-    cargaAlumno.classList.remove('posicionAdelante');
-    
-})
-cargaCurso.addEventListener('click',()=>{
-    cargaCurso.classList.add('posicionAdelante');
-    cargaEscuelas.classList.remove('posicionAdelante');
-    cargaAlumno.classList.remove('posicionAdelante');
-});
-cargaAlumno.addEventListener('click',()=>{
-    cargaAlumno.classList.add('posicionAdelante');
-    cargaCurso.classList.remove('posicionAdelante');
-    cargaEscuelas.classList.remove('posicionAdelante');    
-});
 /* -------------- Pestaña Escuelas --------------------- */
 /* cargar provincias */
 let selectProvincias = document.getElementById('provincia');
